1b6b2aeee030cadf2de62aa0ef66c7426647f96c
Nesurion <maik.schwan@gmail.com>
2015-02-24 16:18:20 +0100

    prep for sinatra

--- hutem.rb
+++ hutem.rb
@@ -1,2 +1,21 @@
 require 'sinatra'
+require 'redis'
 
+set :bind, '0.0.0.0'
+set :port, 8080
+
+redis = Redis.new
+
+helpers do
+  get_current_temp
+    # redis call on latest temp entry here
+  end
+
+  get_current_humidity
+    # redis call on latest humidity enrtry
+  end
+end
+
+get '/' do
+  erb :index
+end
